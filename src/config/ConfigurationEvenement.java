ef454b302784262e406cc20260bf1bd4b83c98b0
Thenuxshan <anutajant@gmail.com>
2026-04-27 22:27:43 +0200

    dernier commit

--- src/config/ConfigurationEvenement.java
+++ src/config/ConfigurationEvenement.java
@@ -3,7 +3,7 @@ package config;
 public class ConfigurationEvenement 
 {
     public static final int PLUIE_IMPACT_DUREE = 4;
-    public static final int PLUIE_IMPACT_TEMPERATURE  = 0;
+    public static final int PLUIE_IMPACT_TEMPERATURE  = -1;
     public static final int PLUIE_IMPACT_HUMIDITE  = 5;
     public static final int PLUIE_IMPACT_POLLUTION  = 0;
     public static final int PLUIE_IMPACT_PURIFICATION  = 0;
@@ -12,10 +12,10 @@ public class ConfigurationEvenement
     public static final int PLUIEACIDE_IMPACT_TEMPERATURE  = 0;
     public static final int PLUIEACIDE_IMPACT_HUMIDITE  = 5;
     public static final int PLUIEACIDE_IMPACT_POLLUTION  = 10;
-    public static final int PLUIEACIDE_IMPACT_PURIFICATION  = 0;
+    public static final int PLUIEACIDE_IMPACT_PURIFICATION  = -10;
     
     public static final int VENTFROID_IMPACT_DUREE = 4;
-    public static final int VENTFROID_IMPACT_TEMPERATURE  = -5;
+    public static final int VENTFROID_IMPACT_TEMPERATURE  = -10;
     public static final int VENTFROID_IMPACT_HUMIDITE  = 0;
     public static final int VENTFROID_IMPACT_POLLUTION  = 0;
     public static final int VENTFROID_IMPACT_PURIFICATION  = 0;
@@ -29,8 +29,8 @@ public class ConfigurationEvenement
     public static final int PURIFICATION_IMPACT_DUREE = 4;
     public static final int PURIFICATION_IMPACT_TEMPERATURE  = 0;
     public static final int PURIFICATION_IMPACT_HUMIDITE  = 0;
-    public static final int PURIFICATION_IMPACT_POLLUTION  = 0;
-    public static final int PURIFICATION_IMPACT_PURIFICATION  = 5;
+    public static final int PURIFICATION_IMPACT_POLLUTION  = -10;
+    public static final int PURIFICATION_IMPACT_PURIFICATION  = 10;
     
     public static double METEORE_PROBABILITE = 0.05;  // 5% de chance
     public static final int METEORE_IMPACT_DUREE = 20;
@@ -43,14 +43,14 @@ public class ConfigurationEvenement
     public static final int POLLUTION_IMPACT_TEMPERATURE  = 0;
     public static final int POLLUTION_IMPACT_HUMIDITE  = 0;
     public static final int POLLUTION_IMPACT_POLLUTION  = 10;
-    public static final int POLLUTION_IMPACT_PURIFICATION  = 0;
+    public static final int POLLUTION_IMPACT_PURIFICATION  = -10;
     
     public static final int PURIFICATION_DUREE = 4;
     public static final int VENT_CHAUD_DUREE = 4;
     
-    public static final int ORAGE_IMPACT_DUREE = 6;
+    public static final int ORAGE_IMPACT_DUREE = 3;
     public static final int ORAGE_IMPACT_TEMPERATURE = 0;
-    public static final int ORAGE_IMPACT_HUMIDITE = 10;
+    public static final int ORAGE_IMPACT_HUMIDITE = 15;
     public static final int ORAGE_IMPACT_POLLUTION = 0;
     public static final int ORAGE_IMPACT_PURIFICATION = 0;
     
@@ -62,25 +62,25 @@ public class ConfigurationEvenement
     
     public static final int TORNADE_IMPACT_DUREE = 8;
     public static final int TORNADE_IMPACT_TEMPERATURE = 0;
-    public static final int TORNADE_IMPACT_HUMIDITE = 5;
+    public static final int TORNADE_IMPACT_HUMIDITE = 0;
     public static final int TORNADE_IMPACT_POLLUTION = 5;
     public static final int TORNADE_IMPACT_PURIFICATION = 0;
     
     public static final int PLUIEBENITE_IMPACT_DUREE = 5;
     public static final int PLUIEBENITE_IMPACT_TEMPERATURE = 0;
     public static final int PLUIEBENITE_IMPACT_HUMIDITE = 8;
-    public static final int PLUIEBENITE_IMPACT_POLLUTION = -5;
+    public static final int PLUIEBENITE_IMPACT_POLLUTION = -10;
     public static final int PLUIEBENITE_IMPACT_PURIFICATION = 10;
     
     public static final int ZEPHYR_IMPACT_DUREE = 3;
     public static final int ZEPHYR_IMPACT_TEMPERATURE = 5;
     public static final int ZEPHYR_IMPACT_HUMIDITE = 0;
-    public static final int ZEPHYR_IMPACT_POLLUTION = 0;
+    public static final int ZEPHYR_IMPACT_POLLUTION = -10;
     public static final int ZEPHYR_IMPACT_PURIFICATION = 10;
     
     public static final int TONNERRE_IMPACT_DUREE = 2;
     public static final int TONNERRE_IMPACT_TEMPERATURE = 0;
-    public static final int TONNERRE_IMPACT_HUMIDITE = 8;
+    public static final int TONNERRE_IMPACT_HUMIDITE = 20;
     public static final int TONNERRE_IMPACT_POLLUTION = 0;
     public static final int TONNERRE_IMPACT_PURIFICATION = 0;
     
@@ -88,7 +88,7 @@ public class ConfigurationEvenement
     public static final int SMOG_IMPACT_TEMPERATURE = 0;
     public static final int SMOG_IMPACT_HUMIDITE = 0;
     public static final int SMOG_IMPACT_POLLUTION = 15;
-    public static final int SMOG_IMPACT_PURIFICATION = 0;
+    public static final int SMOG_IMPACT_PURIFICATION = -15;
     
     public static final int NUAGETOXIQUE_IMPACT_DUREE = 7;
     public static final int NUAGETOXIQUE_IMPACT_TEMPERATURE = 0;
